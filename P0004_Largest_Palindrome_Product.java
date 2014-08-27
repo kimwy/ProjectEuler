d4d003dd4d3083f56f09537fe90b6e1a2385f528
Kim W. <kimwanggaizi7@gmail.com>
2014-08-27 14:37:10 -0700

    better algorithm

--- P0004_Largest_Palindrome_Product.java
+++ P0004_Largest_Palindrome_Product.java
@@ -11,27 +11,34 @@
 public class P0004_Largest_Palindrome_Product {
 	public static void main(String[] args){
 		
-		int max=0;
-		
-		for(int num1=999;num1>100;num1--){
-			for(int num2=999;num2>100; num2--){
-				int result= num1 * num2;
-				if(isPalindrome(result)) {
-					if(result>max) max=result;
+		outermost:
+		for(int result=999*999;result>100000;result--){
+			if(isPalindrome(result)){
+				
+				for(int num1=999;num1>100;num1--){
+					if(result % num1 == 0){
+						int num2=result/num1;
+						if(100<num2 && num2<999){
+							System.out.println(result);
+							break outermost;
+						}
+							
+						}
+					}
 				}
 			}
-		}
-		System.out.println(max);
 		
 	}
 	
 	public static boolean isPalindrome(int number){
-		String numString=number+"";
-		int length=numString.length();
-		for(int i=0;i<length/2; i++){
-			if(numString.charAt(i) != numString.charAt(length-1-i)) return false;
+		int reverse=0;
+		int temp=number;
+		for(int i=0;i<=temp;i++){
+			int remainder=temp%10;
+			reverse = reverse*10 + remainder;
+			temp /= 10;
 		}
-		return true;
+		return reverse == number;
 	}
 	
 }
