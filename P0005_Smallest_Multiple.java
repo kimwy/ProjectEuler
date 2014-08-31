6bf751a4bb63d0aa7e450f403545453be6acb96a
Kim W. <kimwanggaizi7@gmail.com>
2014-08-31 16:10:51 -0700

    faster. math algorithm

--- P0005_Smallest_Multiple.java
+++ P0005_Smallest_Multiple.java
@@ -1,3 +1,4 @@
+
 /*
  * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
  * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
@@ -9,16 +10,41 @@
 public class P0005_Smallest_Multiple {
 	public static void main(String[] args) {
 		
-		int result=20;
-		for(int i=1;;i++){
-			if(result%i != 0){
-				result++;
-				i=0;
-			}
-			if(i==20) break;
+		int bound=20;
+		int[] num=new int[bound];
+		for(int i=0;i<bound;i++){
+			num[i] = i+1;
 		}
-		
-		System.out.println(result);
+		System.out.println(lcm(num));
+	}
+	
+	private static int gcd(int a, int b){
+	    while(b>0){
+	        int temp=b;
+	        b=a%b;
+	        a=temp; //divisor
+	    }
+	    return a;
+	}
+
+//	private static int gcd(int[] nums){
+//	    int result=nums[0];
+//	    for(int i=1; i<nums.length; i++){
+//	    	result = gcd(result, nums[i]);
+//	    }
+//	    return result;
+//	}
+	
+	private static int lcm(int a, int b){
+	    return a * (b / gcd(a, b));
+	}
+
+	private static int lcm(int[] nums){
+	    int result = nums[0];
+	    for(int i= 1; i<nums.length; i++){
+	    	result = lcm(result, nums[i]);
+	    }
+	    return result;
 	}
 
 }
